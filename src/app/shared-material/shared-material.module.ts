7a5f6f8099e3c520ad7c819c3e023c702f1da3fd
Budda Lee <buddalee1stlove@gmail.com>
2018-05-15 10:29:09 -0700

    add sidenav and 調整有關768px ui

--- src/app/shared-material/shared-material.module.ts
+++ src/app/shared-material/shared-material.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatSidenavModule } from '@angular/material';
 
 @NgModule({
-  imports: [], // 先import
-  exports: [] // 在export
+  imports: [MatSidenavModule], // 先import
+  exports: [MatSidenavModule] // 在export
 })
 export class SharedMaterialModule {}
