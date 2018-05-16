8ac30b3ead06cb19f8112d33d9cafcb6040d15b5
Budda Lee <buddalee1stlove@gmail.com>
2018-05-16 09:00:34 -0700

    完成部分1440px ui

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+  sideMode = 'over';
+  isOpend = false;
+  ngOnInit() {
+    if (window.innerWidth >= 1440) {
+      this.sideMode = 'side';
+      this.isOpend = true;
+    }    
+  }
+  onResize(event, sideNav) {
+    if (event.target.innerWidth < 1440) {
+      this.sideMode = 'over';
+      this.isOpend = false;
+    } else {
+      this.sideMode = 'side';
+      this.isOpend = true;
+    }
+  }
+
 }
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
-
+import { wonderReviews } from './fakeData';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
+  reviewDatas = wonderReviews
+  isScale = false;
   constructor() { }
 
   ngOnInit() {
+    if (window.innerWidth >= 1440) {
+      this.isScale = true;
+    }
   }
-
+  onResize(event) {
+    if (event.target.innerWidth < 1440) {
+      this.isScale = false;
+    } else {
+      this.isScale = true;
+    }
+  }
 }
