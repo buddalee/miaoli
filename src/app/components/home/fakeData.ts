9bc13fa7ce063931f5018a92ccf63c11a72df043
Budda Lee <buddalee1stlove@gmail.com>
2018-05-20 08:58:56 -0700

    調整畫面，並把資料變為動態資料顯示

--- src/app/components/home/fakeData.ts
+++ src/app/components/home/fakeData.ts
@@ -10,3 +10,19 @@ export const wonderReviews = [
     time: '2018.05.16'
   }
 ];
+
+export const pttReviews = [
+  {
+    image: "http://pic.qiantucdn.com/58pic/28/44/63/358PICk58PICT58PICS26deF2j5fh_PIC2018.jpg!qt324",
+    description: '空氣新鮮，街道很乾淨 希望下次還有機會造訪'
+  },
+  {
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTNVg1KG-UpfvQjOBFNvkEMu3JTrLGL5sbiEm57CTctvbpuPz3LFQ",
+    description: '超好吃～～'
+  },
+  {
+    image: "http://static.wixstatic.com/media/92b36f_87817020b7dd47f1b7178dcfd7c9d913~mv2.gif",
+    description: '下次要大一家人去吃'
+  }
+];
+
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { wonderReviews } from './fakeData';
+import { wonderReviews, pttReviews } from './fakeData';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -8,6 +8,9 @@ import { wonderReviews } from './fakeData';
 export class HomeComponent implements OnInit {
   reviewDatas = wonderReviews
   isScale = false;
+  pttDatas = pttReviews;
+  pttIdx = 0;
+  reviewIdx = 0;
   constructor() { }
 
   ngOnInit() {
@@ -20,6 +23,34 @@ export class HomeComponent implements OnInit {
       this.isScale = false;
     } else {
       this.isScale = true;
+    }
+  }
+  prePtt() {
+    if (this.pttIdx - 1 > -1) {
+      this.pttIdx--;
+    } else {
+      this.pttIdx = this.pttDatas.length - 1;
+    }
+  }
+  nextPtt() {
+    if (this.pttIdx + 1 !== this.pttDatas.length) {
+      this.pttIdx++;
+    } else {
+      this.pttIdx = 0;
+    }
+  }
+  preReview() {
+    if (this.reviewIdx - 1 > -1) {
+      this.reviewIdx--;
+    } else {
+      this.reviewIdx = this.reviewDatas.length - 1;
+    }
+  }
+  nextReview() {
+    if (this.reviewIdx + 1 !== this.reviewDatas.length) {
+      this.reviewIdx++;
+    } else {
+      this.reviewIdx = 0;
     }
   }
 }
