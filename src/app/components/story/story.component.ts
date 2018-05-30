5c06546213b0d2d8968da3cfef9cf88f6e187236
Budda Lee <buddalee1stlove@gmail.com>
2018-05-30 08:07:12 -0700

    complete bookblock

--- src/app/components/story/story.component.ts
+++ src/app/components/story/story.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Page } from '../../shared/page';
 
 @Component({
@@ -6,64 +6,61 @@ import { Page } from '../../shared/page';
   templateUrl: './story.component.html',
   styleUrls: ['./story.component.css']
 })
-export class StoryComponent implements OnInit, AfterViewInit {
+export class StoryComponent implements OnInit {
   textHiddenNum: number;
   config = {
-    $bookBlock: $('#bb-bookblock'),
-    $navNext: $('#bb-nav-next'),
-    $navPrev: $('#bb-nav-prev'),
+    $bookBlock: $(`#bb-bookblock`),
+    $navNext: $(`#bb-nav-next`),
+    $navPrev: $(`#bb-nav-prev`),
     $navFirst: $('#bb-nav-first'),
     $navLast: $('#bb-nav-last')
   }
   constructor() { }
 
   ngOnInit() {
-    this.init();
+    const arr = [1, 2, 3, 4];
+    arr.forEach(_arr => this.init(_arr));
   }
-  ngAfterViewInit()	{
-    // this.init();
-  }
-  init() {
-    console.log($('#bb-bookblock'));
-    $('#bb-bookblock').bookblock( {
+  init(i) {
+    $(`#bb-bookblock${i}`).bookblock( {
       speed : 800,
       shadowSides : 0.8,
       shadowFlip : 0.7
     } );
-    this.initEvents();
+    this.initEvents(i);
   }
-  initEvents() {
-    var $slides = $('#bb-bookblock').children();
+  initEvents(i) {
+    var $slides = $(`#bb-bookblock${i}`).children();
 
     // add navigation events
-    $('#bb-nav-next').on( 'click touchstart', () => {
-      $('#bb-bookblock').bookblock( 'next' );
+    $(`#bb-nav-next${i}`).on( 'click touchstart', () => {
+      $(`#bb-bookblock${i}`).bookblock( 'next' );
       return false;
     } );
 
-    $('#bb-nav-prev').on( 'click touchstart', () => {
-      $('#bb-bookblock').bookblock( 'prev' );
+    $(`#bb-nav-prev${i}`).on( 'click touchstart', () => {
+      $(`#bb-bookblock${i}`).bookblock( 'prev' );
       return false;
     } );
 
     $('#bb-nav-first').on( 'click touchstart', () => {
-      $('#bb-bookblock').bookblock( 'first' );
+      $(`#bb-bookblock${i}`).bookblock( 'first' );
       return false;
     } );
 
     $('#bb-nav-last').on( 'click touchstart', () => {
-      $('#bb-bookblock').bookblock( 'last' );
+      $(`#bb-bookblock${i}`).bookblock( 'last' );
       return false;
     } );
     
     // add swipe events
     $slides.on( {
       'swipeleft' : ( event ) => {
-        $('#bb-bookblock').bookblock( 'next' );
+        $(`#bb-bookblock${i}`).bookblock( 'next' );
         return false;
       },
       'swiperight' : ( event ) => {
-        $('#bb-bookblock').bookblock( 'prev' );
+        $(`#bb-bookblock${i}`).bookblock( 'prev' );
         return false;
       }
     } );
@@ -80,10 +77,10 @@ export class StoryComponent implements OnInit, AfterViewInit {
 
       switch (keyCode) {
         case arrow.left:
-          $('#bb-bookblock').bookblock( 'prev' );
+          $(`#bb-bookblock${i}`).bookblock( 'prev' );
           break;
         case arrow.right:
-          $('#bb-bookblock').bookblock( 'next' );
+          $(`#bb-bookblock${i}`).bookblock( 'next' );
           break;
       }
     } );
