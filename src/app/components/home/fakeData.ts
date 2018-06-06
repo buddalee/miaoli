c00c875c69d2d9a9c07d9c8b8011ae3c5c4cbcf0
Budda Lee <buddalee1stlove@gmail.com>
2018-06-06 08:12:23 -0700

    優化圖片

--- src/app/components/home/fakeData.ts
+++ src/app/components/home/fakeData.ts
@@ -1,12 +1,12 @@
 export const wonderReviews = [
   {
-    image: "https://s.zimedia.com.tw/s/NT2gmf-0",
+    image: "assets/photo/food1.jpeg",
     description: '客家老味道，橘餅饅頭',
     time: '2017.09.20'
   },
   {
-    image: "http://iphoto.ipeen.com.tw/photo/comment/444208/753146/cgm5bf135cd171d569d5842ce3d61eb8b3b155.jpg",
-    description: '客家老味道，？麵',
+    image: "assets/photo/food2.jpg",
+    description: '客家老味道，麵',
     time: '2018.05.16'
   }
 ];
